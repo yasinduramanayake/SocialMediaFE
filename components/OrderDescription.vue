095d7aa9832f0f473f46cd6f81967d322fa9b897
yasinduyuga <yasindu@smartedesigners.com>
2023-11-06 22:51:53 +0530

    updated

--- components/OrderDescription.vue
+++ components/OrderDescription.vue
@@ -1,91 +1,97 @@
 <template>
   <div>
     <b-card>
+      <div
+        class="pt-3"
+        style="
+          border-left: 1px solid rgb(220, 220, 220);
+          height: 50px;
+          float: right;
+        "
+      >
+        <span class="ml-2 font-weight-bold"> $12.99</span>
+      </div>
+
       <b-container class="order_services_paddings">
-        <div class="row no-gutters">
-          <b-col sm="2" cols="3">
+        <b-row no-gutters>
+          <b-col
+            sm="7"
+            md="9"
+            cols="12"
+            class="d-flex justify-content-start pt-2"
+          >
             <b-avatar
               variant="none"
               size="sm"
               src="https://buysocialmediamarketing.com/img/inst-user.svg"
             ></b-avatar>
-          </b-col>
-          <b-col sm="8" cols="6">
+            &nbsp;
             <div class="font-weight-bold">1000 Followers</div>
           </b-col>
 
-          <b-col sm="8" cols="6">
-            <div
-              data-v-5e9d276a=""
-              data-v-53e947c8=""
-              data-select=""
-              class="custom-select functioned"
-              style="
-                display: inline-flex;
-                flex-direction: column;
-                vertical-align: middle;
-              "
+          <b-col sm="5" md="3" cols="6">
+            <b-dropdown
+              id="dropdown-1"
+              class="m-md-2"
+              variant="primary"
+              :text="quality"
             >
-              <!---->
-              <button
-                data-v-5e9d276a=""
-                type="button"
-                data-select-btn=""
-                class="custom-select__btn functioned btn--options"
+              <b-dropdown-item @click="qualityChange1()"
+                >High Quality</b-dropdown-item
               >
-                <span data-v-5e9d276a="" data-select-current="" class="sp">
-                  Real
-                </span>
-                <span data-v-5e9d276a="" class="product-title-btn-icon"
-                  ><svg
-                    data-v-5e9d276a=""
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                  >
-                    <path
-                      data-v-5e9d276a=""
-                      d="M11.8891 7.5533C12.78 7.5533 13.2262 8.63044 12.5962 9.26041L9.70711 12.1495C9.31658 12.54 8.68342 12.54 8.29289 12.1495L5.40381 9.26041C4.77384 8.63044 5.22001 7.5533 6.11091 7.5533L11.8891 7.5533Z"
-                      fill="#04a5da"
-                      fill-opacity="1"
-                    ></path></svg
-                ></span>
-              </button>
-              <div
-                data-v-5e9d276a=""
-                data-select-drop=""
-                class="custom-select__dropdown"
-                style="min-width: fit-content; padding-right: 0px"
+              <b-dropdown-item @click="qualityChange2()">Real</b-dropdown-item>
+            </b-dropdown>
+          </b-col>
+
+          <b-col md="12" cols="12" class="d-flex justify-content-start pt-2">
+            <b-button
+              variant="outline-light"
+              style="outline: auto; outline-color: rgb(220, 220, 220)"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
               >
-                <ul data-v-5e9d276a="" class="custom-select__list">
-                  <li data-v-5e9d276a="" class="custom-select__item">
-                    <a
-                      data-v-5e9d276a=""
-                      data-select-option7=""
-                      class=""
-                      style="white-space: nowrap"
-                    >
-                      High Quality
-                    </a>
-                  </li>
-                  <li data-v-5e9d276a="" class="custom-select__item">
-                    <a
-                      data-v-5e9d276a=""
-                      data-select-option7=""
-                      class="current"
-                      style="white-space: nowrap"
-                    >
-                      Real
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div></b-col
-          >
-        </div>
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"
+                /></svg
+            ></b-button>
+
+            <span class="ml-3 font-weight-bold">1000</span>
+            <b-button
+              class="ml-3"
+              variant="outline-light"
+              style="outline: auto; outline-color: rgb(220, 220, 220)"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+                /></svg
+            ></b-button>
+          </b-col>
+        </b-row>
       </b-container>
+
+      <hr />
+
+      <br />
+      <b-card style="border-radius: 10px">
+        <b-row class="d-flex justify-content-start">
+          <b-avatar></b-avatar>
+
+          <span class="font-weight-bold ml-4"> Name</span>
+        </b-row>
+
+        <b-row class="d-flex justify-content-start ml-5">
+        <span class="font-weight-bold">657</span> &nbsp; Posts <span class="font-weight-bold ml-3">650 </span>  &nbsp;Following
+        <span class="font-weight-bold ml-3">150 027</span>  &nbsp;Followers
+      </b-row>
+      </b-card>
     </b-card>
   </div>
 </template>
@@ -93,6 +99,7 @@
 export default {
   data() {
     return {
+      quality: 'High Quality',
       selected: 'hq',
       options: [
         {
@@ -105,6 +112,15 @@ export default {
         },
       ],
     }
+  },
+
+  methods: {
+    qualityChange1() {
+      this.quality = 'High Quality'
+    },
+    qualityChange2() {
+      this.quality = 'Real'
+    },
   },
 }
 </script>
