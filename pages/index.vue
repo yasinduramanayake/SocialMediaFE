2777b44f0eef7451577205499662561a9e32b68e
Yasindu <yasindu@sp-solutions.biz>
2023-12-02 11:53:03 +0530

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -18,8 +18,28 @@
     <Freetrail />
     <br /><br /><br />
     <br /><br /><br />
-    <br /><br /><br />
-    <FooterComponent />
+    <br class="br_tab_view"/><br class="br_tab_view" /><br class="br_tab_view"/>
+    <br class="br_tab_view"/><br class="br_tab_view" /><br class="br_tab_view"/>
+    <br class="br_tab_view"/><br class="br_tab_view" /><br class="br_tab_view"/>
+    <br class="br_tab_view"/><br class="br_tab_view" /><br class="br_tab_view"/>
+    <div class="banner">
+      <FooterComponent />
+      <b-card class="card-container border-0 bg-footer-image">
+        <b-row>
+          <b-col lg="5" md="12" sm="12">
+            <b-card-text class="pt-4 ml-5 text-white h2 card_text">
+              Get Started <br />
+              Now!
+            </b-card-text>
+          </b-col>
+          <b-col lg="7" md="12" sm="12">
+            <div class="pt-3 mr-4">
+              <SocailMediaComponent  />
+            </div>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -34,3 +54,4 @@ export default {
   components: { BuyServices, Advantages, Freetrail, OurServices },
 }
 </script>
+<style></style>
